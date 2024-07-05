3a62fdbcd52de0cf0f24bce1c5c6527adadee5f0
chanaijun <Derikechan@163.com>
2024-07-05 14:31:53 +0800

    新建项目

---
+++ src/main/java/org/tech/springcode/AdminTechApplication.java
@@ -0,0 +1,13 @@
+package org.tech.springcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tech/springcode/SpringCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tech.springcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
